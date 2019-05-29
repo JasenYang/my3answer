d73cd2f14e00d95e82cc28fdeda85c5ccfcc6b0c
JasenYang <2296685742@qq.com>
2019-05-29 22:31:04 +0800

    hh

--- main/main.js
+++ main/main.js
@@ -1,4 +1,14 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    distance = inputs['distance']
+    parkTime = inputs['parkTime']
+    summary1 = parkTime * 0.25
+    if (distance <= 2) {
+      summary2 = 6
+    } else if (distance <= 8) {
+      summary2 = 6 + 0.8 * (distance - 2)
+    } else {
+      summary2 = 10.8 + 1.2 * (distance - 8)
+    }
+    return Math.round(summary1 + summary2);
 };
